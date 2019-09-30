0bea5097bb4acd17c81fb7daaf20388abb5030f8
Daniel Petrov <daniel221569@gmail.com>
2019-09-30 18:04:11 +0200

    Refactoring

---
+++ src/taskpane/CitationFinder.ts
@@ -0,0 +1,45 @@
+// Passive Citation
+// (Doe, 2019) - 1 author
+// (Doe and Borg, 2019) - 2 authors
+// (Joe, Doe and Borg, 2019) - 3 authors
+// (Doe et al., 2019) - 4+ authors
+
+// Active Citation
+// Doe (2019) - 1 author
+// Doe and Borg (2019) - 2 authors
+// Joe, Doe and Borg (2019) - 3 authors
+// Doe et al. (2019) - 4+ authors
+
+// ???
+// https://guides.libraries.psu.edu/apaquickguide/intext
+// multiple citations, e.g. (Derwing, Rossiter, & Munro, 2002; Thomas, 2004)
+// page number, e.g. (Field, 2005, p. 14)
+// not dated, e.g (Smith, n.d.)
+
+const activeCitationRegExp: RegExp = /([A-z.]+(, | and | et al\.)?){1,3} \(\d{4}\)/g;
+const passiveCitationRegExp: RegExp = /\(([A-z ,.&]+, \d{4}(; )?)+\)/g;
+
+class CitationFinder {
+
+  private _all: string[];
+  private _unique: Set<string>;
+
+  constructor(private text: string) {
+    this._all = [...this.text.match(passiveCitationRegExp), ...this.text.match(activeCitationRegExp)];
+    this._unique = new Set(this._all.map(citation => citation.replace(/\(|\)|,/g, '')));
+  }
+
+  get total(): number {
+    return this._all.length;
+  }
+
+  get unique(): number {
+    return this._unique.size;
+  }
+
+  get htmlMessage(): string {
+    return `Found <strong>${this.total}</strong> total (<strong>${this.unique}</strong> unique) in-text citation${this.total === 1 ? '' : 's'}.`;
+  }
+}
+
+export default CitationFinder;
--- src/taskpane/TaskpaneManager.ts
+++ src/taskpane/TaskpaneManager.ts
@@ -1,29 +1,9 @@
+import CitationFinder from './CitationFinder';
+
 // use JavaScript regex to find the culprits
 // then search specifically for them using the word api (and highlight)
 
-// Passive Citation
-// (Doe, 2019) - 1 author
-// (Doe and Borg, 2019) - 2 authors
-// (Joe, Doe and Borg, 2019) - 3 authors
-// (Doe et al., 2019) - 4+ authors
-
-const activeCitationRegExp: RegExp = /([A-z.]+(, | and | et al\.)?){1,3} \(\d{4}\)/g;
-const passiveCitationRegExp: RegExp = /\(([A-z ,.&]+, \d{4}(; )?)+\)/g;
-
-// Active Citation
-// Doe (2019) - 1 author
-// Doe and Borg (2019) - 2 authors
-// Joe, Doe and Borg (2019) - 3 authors
-// Doe et al. (2019) - 4+ authors
-
-// ???
-// https://guides.libraries.psu.edu/apaquickguide/intext
-// multiple citations, e.g. (Derwing, Rossiter, & Munro, 2002; Thomas, 2004)
-// page number, e.g. (Field, 2005, p. 14)
-// not dated, e.g (Smith, n.d.)
-
 class TaskpaneManager {
-
   bind() {
     document.getElementById("btn-count").onclick = this.count;
     document.getElementById("btn-highlight").onclick = this.highlight;
@@ -34,16 +14,8 @@ class TaskpaneManager {
       context.document.body.load('text');
       await context.sync();
 
-      const text: string = context.document.body.text;
-      const citations = [
-        ...text.match(passiveCitationRegExp),
-        ...text.match(activeCitationRegExp)
-      ].map(citation => citation.replace(/\(|\)|,/g, ''));
-      const set = new Set(citations);
-
-      const countSummary = document.getElementById('count-summary');
-      countSummary.innerHTML =
-        `Found <strong>${citations.length}</strong> total (<strong>${set.size}</strong> unique) in-text citation${citations.length === 1 ? '' : 's'}.`;
+      const finder = new CitationFinder(context.document.body.text);
+      document.getElementById('count-summary').innerHTML = finder.htmlMessage;
 
       await context.sync();
     }).catch(console.log);
