0a511792e48b5ae89d4b77ddaf6d7738258e61c6
Daniel Petrov <daniel221569@gmail.com>
2019-10-01 21:06:53 +0200

    Add more tests

--- src/taskpane/functions.test.ts
+++ src/taskpane/functions.test.ts
@@ -1,5 +1,11 @@
 import { findCitations, findUnique } from './functions';
 
+// ???
+// https://guides.libraries.psu.edu/apaquickguide/intext
+// multiple citations, e.g. (Derwing, Rossiter, & Munro, 2002; Thomas, 2004)
+// page number, e.g. (Field, 2005, p. 14)
+// not dated, e.g (Smith, n.d.)
+
 describe('findCitations', () => {
   test('when given empty string, should return empty array', () => {
     // Act
@@ -26,6 +32,60 @@ describe('findCitations', () => {
     expect(citations.length).toBe(1);
     expect(citations[0]).toBe('Doe (2019)');
   });
+
+  test('should match passive double author citation', () => {
+    // Act
+    const citations = findCitations('Jest is a delightful JavaScript Testing Framework with a focus on simplicity (Doe and Borg, 2019).');
+
+    // Act
+    expect(citations.length).toBe(1);
+    expect(citations[0]).toBe('(Doe and Borg, 2019)');
+  });
+
+  test('should match active double author citation', () => {
+    // Act
+    const citations = findCitations('Doe and Borg (2019) claim that Jest is a delightful JavaScript Testing Framework with a focus on simplicity.');
+
+    // Assert
+    expect(citations.length).toBe(1);
+    expect(citations[0]).toBe('Doe and Borg (2019)');
+  });
+
+  test('should match passive triple author citation', () => {
+    // Act
+    const citations = findCitations('Jest is a delightful JavaScript Testing Framework with a focus on simplicity (Joe, Doe and Borg, 2019).');
+
+    // Act
+    expect(citations.length).toBe(1);
+    expect(citations[0]).toBe('(Joe, Doe and Borg, 2019)');
+  });
+
+  test('should match active triple author citation', () => {
+    // Act
+    const citations = findCitations('Joe, Doe and Borg (2019) claim that Jest is a delightful JavaScript Testing Framework with a focus on simplicity.');
+
+    // Assert
+    expect(citations.length).toBe(1);
+    expect(citations[0]).toBe('Joe, Doe and Borg (2019)');
+  });
+
+  test('should match passive et al. citation', () => {
+    // Act
+    const citations = findCitations('Jest is a delightful JavaScript Testing Framework with a focus on simplicity (Doe et al., 2019).');
+
+    // Act
+    expect(citations.length).toBe(1);
+    expect(citations[0]).toBe('(Doe et al., 2019)');
+  });
+
+  test('should match active et al. author citation', () => {
+    // Act
+    const citations = findCitations('Doe et al. (2019) claim that Jest is a delightful JavaScript Testing Framework with a focus on simplicity.');
+
+    // Assert
+    expect(citations.length).toBe(1);
+    expect(citations[0]).toBe('Doe et al. (2019)');
+  });
 });
 
 describe('findUnique', () => {
@@ -35,5 +95,21 @@ describe('findUnique', () => {
 
     // Assert
     expect(unique.size).toBe(0);
+  });
+
+  test('when given single reference, should return single element set', () => {
+    // Act
+    const unique = findUnique(['Doe (2019)']);
+
+    // Assert
+    expect(unique.size).toBe(1);
+  });
+
+  test('when given two references to the same source in a different format, should return single element set', () => {
+    // Act
+    const unique = findUnique(['Doe (2019)', '(Doe, 2019)']);
+
+    // Assert
+    expect(unique.size).toBe(1);
   });
 });
--- src/taskpane/functions.ts
+++ src/taskpane/functions.ts
@@ -1,27 +1,5 @@
-// Passive Citation
-// (Doe, 2019) - 1 author
-// (Doe and Borg, 2019) - 2 authors
-// (Joe, Doe and Borg, 2019) - 3 authors
-// (Doe et al., 2019) - 4+ authors
-
-// Active Citation
-// Doe (2019) - 1 author
-// Doe and Borg (2019) - 2 authors
-// Joe, Doe and Borg (2019) - 3 authors
-// Doe et al. (2019) - 4+ authors
-
-// ???
-// https://guides.libraries.psu.edu/apaquickguide/intext
-// multiple citations, e.g. (Derwing, Rossiter, & Munro, 2002; Thomas, 2004)
-// page number, e.g. (Field, 2005, p. 14)
-// not dated, e.g (Smith, n.d.)
-
-// const activeCitationRegExp: RegExp = /([A-z.]+(, | and | et al\.)?){1,3} \(\d{4}\)/g;
-// const passiveCitationRegExp: RegExp = /\(([A-z ,.&]+, \d{4}(; )?)+\)/g;
-const citationRegex: RegExp = /(([A-z.]+(, | and | et al\.)?){1,3} \(\d{4}\))|(\(([A-z ,.&]+, \d{4}(; )?)+\))/g;
-
 export function findCitations(text: string): ReadonlyArray<string> {
-  return text.match(citationRegex) || [];
+  return text.match(/(([A-z.]+(, | and | et al\.)?){1,3} \(\d{4}\))|(\(([A-z ,.&]+, \d{4}(; )?)+\))/g) || [];
 }
 
 export function findUnique(citations: ReadonlyArray<string>): ReadonlySet<string> {
