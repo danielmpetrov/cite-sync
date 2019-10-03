8541dad7124b5a7721e53a1f16cc88356176422c
Daniel Petrov <daniel221569@gmail.com>
2019-10-03 20:04:00 +0200

    Display a list of citations that were not referenced (with link to highlight)

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -11,6 +11,7 @@ Office.onReady(info => {
     document.getElementById('btn-analyze').onclick = analyze;
     summary = document.getElementById('summary');
     extraInfo = document.getElementById('extra-info');
+    extraInfo.onclick = lookup;
   }
 });
 
@@ -29,28 +30,6 @@ async function count() {
   }).catch(console.log);
 }
 
-let current: number = 0;
-
-async function highlight() {
-  return Word.run(async context => {
-    context.document.body.load('text');
-    await context.sync();
-
-    const citations = findCitations(context.document.body.text);
-
-    if (current === citations.length) {
-      current = 0;
-    }
-
-    const citation = citations[current++];
-    const result = context.document.body.search(citation).load('text');
-    await context.sync();
-
-    result.items[0].select();
-    await context.sync();
-  }).catch(console.log);
-}
-
 async function analyze() {
   return Word.run(async context => {
     const wordParagraphs = context.document.body.paragraphs.load('text');
@@ -61,12 +40,37 @@ async function analyze() {
     const orphanedReferences = findOrphanedReferences(paragraphs, references);
     const orphanedCitations = findOrphanedCitations(paragraphs, references);
 
-    console.log(orphanedReferences);
-    console.log(orphanedCitations);
     summary.innerHTML = `Found <strong>${orphanedReferences.length}</strong> reference(s) that were never cited in-text.`;
     extraInfo.innerHTML = '';
     for (const reference of orphanedReferences) {
       extraInfo.innerHTML += `<p>${reference}</p>`
     }
+    extraInfo.innerHTML += `<p style="text-align: center; margin-bottom: 0;">
+      Found <strong>${orphanedCitations.length}</strong> (<strong>${findUnique(orphanedCitations).size}</strong> unique) citation(s) that were not referenced.
+    </p>`;
+
+    for (const citation of orphanedCitations) {
+      extraInfo.innerHTML += `
+        <div role="button" class="ms-welcome__action ms-Button ms-Button--hero ms-font-sm">
+          <span class="ms-Button-label citation-link">${citation}</span>
+        </div>
+      `;
+    }
   });
+}
+
+async function lookup(event: any) {
+  if (!event.target.classList.contains('citation-link')) {
+    return;
+  }
+
+  const citation = event.target.innerText.trim();
+
+  return Word.run(async context => {
+    const result = context.document.body.search(citation).load('text');
+    await context.sync();
+
+    result.items[0].select();
+    await context.sync();
+  }).catch(console.log);
 }
