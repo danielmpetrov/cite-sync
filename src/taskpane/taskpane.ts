a813058d511de5027e335eb53cea04da3d28c539
Daniel Petrov <daniel221569@gmail.com>
2019-10-03 11:25:42 +0200

    WIP - Display results

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -1,11 +1,16 @@
-import { findCitations, findUnique, htmlMessage, parseWordParagraphs } from './functions';
+import { findCitations, findUnique, htmlMessage, parseWordParagraphs, findOrphanedReferences, findOrphanedCitations } from './functions';
+
+let summary: HTMLElement;
+let extraInfo: HTMLElement;
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Word) {
     document.getElementById('sideload-msg').style.display = 'none';
     document.getElementById('app-body').style.display = 'flex';
     document.getElementById('btn-count').onclick = count;
-    document.getElementById('btn-highlight').onclick = findReferences;
+    document.getElementById('btn-analyze').onclick = analyze;
+    summary = document.getElementById('summary');
+    extraInfo = document.getElementById('extra-info');
   }
 });
 
@@ -17,7 +22,8 @@ async function count() {
     const citations = findCitations(context.document.body.text);
     const unique = findUnique(citations);
     const message = htmlMessage(citations.length, unique.size);
-    document.getElementById('count-summary').innerHTML = message;
+    summary.innerHTML = message;
+    extraInfo.innerHTML = '';
 
     await context.sync();
   }).catch(console.log);
@@ -45,13 +51,22 @@ async function highlight() {
   }).catch(console.log);
 }
 
-async function findReferences() {
+async function analyze() {
   return Word.run(async context => {
     const wordParagraphs = context.document.body.paragraphs.load('text');
     await context.sync();
 
     const [paragraphs, references] = parseWordParagraphs(wordParagraphs.items);
 
-    console.log(paragraphs, references);
+    const orphanedReferences = findOrphanedReferences(paragraphs, references);
+    const orphanedCitations = findOrphanedCitations(paragraphs, references);
+
+    console.log(orphanedReferences);
+    console.log(orphanedCitations);
+    summary.innerHTML = `Found <strong>${orphanedReferences.length}</strong> reference(s) that were never cited in-text.`;
+    extraInfo.innerHTML = '';
+    for (const reference of orphanedReferences) {
+      extraInfo.innerHTML += `<p>${reference}</p>`
+    }
   });
 }
